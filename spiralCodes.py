d749ef53b8b737948c29aab445f08c81928e457f
Oladejo Abdullahi <67700908+maxwizard01@users.noreply.github.com>
2021-09-06 00:30:51 +0100

    Add files via upload

---
+++ spiralCodes.py
@@ -0,0 +1,20 @@
+def spiral(times,angleSpace,length,breadth,color):
+    import turtle
+    spiral = turtle.Turtle()
+    spiral.speed(10)
+    spiral.fillcolor(color)
+    spiral.begin_fill()
+    for i in range(times):
+        spiral.forward(length)
+        spiral.right(10.5)
+        spiral.forward(breadth)
+        spiral.left(30)
+        spiral.forward(breadth)
+        spiral.right(30)   
+        spiral.penup()
+        spiral.setposition(0, 0)
+        spiral.pendown()
+        spiral.right(angleSpace)
+    spiral.end_fill()
+    turtle.done()
+spiral(180,60,80,30,"#53a4a9")
